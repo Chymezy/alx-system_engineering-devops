0d3130ce67d224a482e694c8b38c6afabb06875b
root <chymezy332@gmail.com>
2024-04-06 05:37:32 -0700

    removed exec layer and maintain package manager althrough

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,22 +1,16 @@
-package { 'python3':
+package { ['python3', 'python3-pip']:
   ensure => 'installed',
 }
 
-exec { 'install_pip3':
-  command => '/usr/bin/apt-get -y install python3-pip',
-  unless  => '/usr/bin/dpkg -l python3-pip',
-  require => Package['python3'],
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  environment => ['PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-  unless      => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require     => Exec['install_pip3'],
+package { 'flask':
+  ensure    => '2.1.0',
+  provider  => 'pip',
+  require   => Package['python3-pip'],
 }
 
 package { 'werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3'
+  ensure    => '2.1.1',
+  provider  => 'pip',
+  require   => Package['python3-pip'],
 }
 
